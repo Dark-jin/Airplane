b8670c617c44747ee1d4c3959aa7f0340e4dae47
Dark-jin <chch4lee1@naver.com>
2023-04-05 19:30:51 +0900

    feat: types lottie

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,7 @@
 import animationloading from "../assets/97952-loading-animation-blue.json";
+import Lottie from 'react-lottie';
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return(<Lottie)
 };
 export default Loading;
